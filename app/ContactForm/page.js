fadf59364f9d6985e650b11ad75f6ad45106a0c1
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-06-15 16:44:46 +0530

    add toast message to show that form is submited successfully

--- app/ContactForm/page.js
+++ app/ContactForm/page.js
@@ -8,17 +8,15 @@ export default function ContactForm() {
     const [email, setEmail] = useState("")
     const [phoneNumber, setPhoneNumber] = useState("")
     const [message, setMessage] = useState("")
+    const [showToast, setShowToast] = useState(false)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://localhost:5000/contact', {
                 name, email, phoneNumber, message
-            })
+            });
             console.log(response);
-            
-            // Log the state before clearing
-            console.log("Before clearing: ", { name, email, phoneNumber, message });
             
             // Clear the form fields after successful submission
             setName("");
@@ -26,15 +24,18 @@ export default function ContactForm() {
             setPhoneNumber("");
             setMessage("");
 
-            // Log the state after clearing
-            console.log("After clearing: ", { name, email, phoneNumber, message });
+            // Show the toast
+            setShowToast(true);
+
+            // Hide the toast after 3 seconds
+            setTimeout(() => setShowToast(false), 5000);
         } catch (error) {
             console.log(error.message);
         }
     }
 
     return (
-        <div className='border max-w-4xl mx-auto mt-12 rounded-lg'>
+        <div className='border max-w-4xl mx-auto mt-12'>
             <h1 className='text-center py-7 font-mono text-3xl underline drop-shadow-xl '>Contact Form</h1>
             <form onSubmit={handleSubmit} className='flex flex-col gap-5 max-w-xl mx-auto pb-12 px-10 '>
                 <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name" className="input input-bordered w-full max-w-xl" />
@@ -43,6 +44,14 @@ export default function ContactForm() {
                 <textarea value={message} onChange={(e) => setMessage(e.target.value)} className="textarea text-lg" placeholder="Enter your message"></textarea>
                 <input type="submit" value="Submit" className="btn" />
             </form>
+
+            {showToast && (
+                <div className="toast toast-top toast-end">
+                    <div className="alert alert-success">
+                        <span>Message sent successfully.</span>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
