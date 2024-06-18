870b2f9891e43cd3f6a19555452f926cc06ca9c6
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-06-18 17:55:48 +0530

    add footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./Navigation/page";
+import Footer from "./Footer/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navigation/>
         {children}
+        <Footer />
       </body>
     </html>
   );
