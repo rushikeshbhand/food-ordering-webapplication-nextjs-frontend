d6168e6a7d82191d5fb106db5ef13e2a4bc0d63b
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-06-18 17:56:56 +0530

    create signup page

--- app/Signup/page.js
+++ app/Signup/page.js
@@ -1,7 +1,106 @@
-import React from 'react'
+'use client'
+
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Signup() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:5000/createUser', {
+        username,
+        email,
+        password
+      })
+      const {message, token, createdUser} = response.data; //In axios response comes inside data object
+      console.log(`message and token and created user is : ${message} and ${token} and user is ${JSON.stringify(createdUser)}`)
+
+      // store the token in the local storage 
+      localStorage.setItem('token', token)
+      alert(`${response.data.createdUser.username} is successfully registered`)
+    }
+    
+    catch (err) {
+      console.log(err.message);
+      alert(`enter all credentials to signup`)
+
+    }
+  }
+
   return (
-    <div>Signup</div>
-  )
+    <section className="bg-white dark:bg-gray-900">
+      <div className="flex justify-center min-h-screen">
+        <div
+          className="hidden bg-cover lg:block lg:w-2/5"
+          style={{
+            backgroundImage: `url('https://cdn.prod.website-files.com/5e86c7170f1ab21474c3f2a4/5ef1d04b4056011f67df2ed7_Natural%2Blight%2B-%2BFood%2BPhotography%2B-%2BFrenchly%2B-4365.jpg')`,
+          }}
+        ></div>
+
+        <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+          <div className="w-full">
+            <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
+              Get your free account now.
+            </h1>
+
+            <p className="mt-4 text-gray-500 dark:text-gray-400">
+              Let’s get you all set up so you can verify your personal account and begin setting up your profile.
+            </p>
+            <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6 mt-8 ">
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">username</label>
+                <input
+                  type="text"
+
+                  value={username}
+                  onChange={(e) => { setUsername(e.target.value) }}
+                  placeholder="Enter username"
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => { setEmail(e.target.value) }}
+                  placeholder="johnsnow@example.com"
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Password</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => { setPassword(e.target.value) }}
+                  placeholder="Enter your password"
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+
+              <button className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                <span>Sign Up </span>
+
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 rtl:-scale-x-100" viewBox="0 0 20 20" fill="currentColor">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
