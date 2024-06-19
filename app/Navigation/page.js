9b79ba14d7c5a1dd4342d431d731f464a99fb31f
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-06-19 17:06:08 +0530

    add cart path in navbar to navigate to path

--- app/Navigation/page.js
+++ app/Navigation/page.js
@@ -11,7 +11,7 @@ export default function Navigation() {
   };
 
   return (
-    <div>
+    <div className="sticky top-0 z-50">
       {/* Desktop view for navbar */}
       <div className="navbar bg-base-100 hidden lg:flex">
         <div className="navbar-start">
@@ -21,9 +21,9 @@ export default function Navigation() {
           <ul className="menu menu-horizontal px-1">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/ShowProducts">Get your favourite food</Link></li>
-            <li><Link href="/">Cart</Link></li>
+            <li><Link href="/Cart">Cart</Link></li>
             <li><Link href="/">Orders</Link></li>
-            <li><Link href="/ContactForm">Contact Form</Link></li>
+            <li><Link href="/ContactForm">Contact Us</Link></li>
             <li><Link href="/Login">Login</Link></li>
             <li><Link href="/Signup">Signup</Link></li>
           </ul>
@@ -36,24 +36,24 @@ export default function Navigation() {
           <a className="btn btn-ghost normal-case text-xl">Food Wala</a>
           <button className="btn btn-square btn-ghost" onClick={handleOnClick}>
             {isOpen ? (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
             ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
             )}
           </button>
         </div>
         {isOpen && (
-          <div className="navbar bg-base-100">
-            <ul className="menu menu-vertical p-2">
+          <div className="bg-base-100 sticky top-0 z-50">
+            <ul className="flex flex-col gap-4 text-center p-2">
               <li><Link href="/">Home</Link></li>
               <li><Link href="/ShowProducts">Get your favourite food</Link></li>
-              <li><Link href="/">Cart</Link></li>
+              <li><Link href="/Cart">Cart</Link></li>
               <li><Link href="/">Orders</Link></li>
-              <li><Link href="/ContactForm">Contact Form</Link></li>
+              <li><Link href="/ContactForm">Contact Us</Link></li>
               <li><Link href="/Login">Login</Link></li>
               <li><Link href="/Signup">Signup</Link></li>
             </ul>
