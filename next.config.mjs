f5858db97d50cfdb9835281eb0cbf0bfe2034d85
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-06-19 18:03:55 +0530

    Configure deployment to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['img.daisyui.com', 'encrypted-tbn0.gstatic.com', 'img.freepik.com', 'static.vecteezy.com', 'www.archanaskitchen.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'img.daisyui.com',
+      'encrypted-tbn0.gstatic.com',
+      'img.freepik.com',
+      'static.vecteezy.com',
+      'www.archanaskitchen.com',
+    ],
+    unoptimized: true, // Disable image optimization for GitHub Pages
+  },
+  basePath: '/food-ordering-webapplication-nextjs-frontend',
+  assetPrefix: '/food-ordering-webapplication-nextjs-frontend/',
+  output: 'export', // Add this line for static export
+};
+
+export default nextConfig;
