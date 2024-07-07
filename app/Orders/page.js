f902e2a4a0e70d8587e64f351d0b5e52019a4549
Rushikesh Bhand <112516622+rushikeshbhand@users.noreply.github.com>
2024-07-07 13:48:14 +0530

    add stype for orders page

--- app/Orders/page.js
+++ app/Orders/page.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export default function Orders() {
   return (
-    <div>Orders</div>
-  )
+    <div className='min-h-screen flex items-center justify-center bg-gradient-to-r from-slate-300 to-purple-600'>
+      <div className='bg-white shadow-lg rounded-lg max-w-4xl w-full mx-5'>
+        <h1 className='text-center text-4xl text-gray-800 pt-8 font-extrabold border-b-2 border-gray-200 pb-4'>
+          Your Orders
+        </h1>
+        <div className='p-6'>
+          <p className='text-center text-gray-600'>
+            Here are the details of your recent orders.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
